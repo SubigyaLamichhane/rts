e377edc545bc894d476987e40391a9345e86ed92
subigyalamichhane <subigyalamichhane@gmail.com>
2022-05-22 19:35:48 +0545

    ahvoewan

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import ReactDOM from 'react-dom';
 
-const 
-App = () => {
-  return <div>
-    <h1>Hi There</h1>
-  </div>};
-
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
+const App = () => {
+  return (
+    <div>
+      <h1>Hi There</h1>
+    </div>
   );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
